f189d08fc1782456ac2f3dc650d943973a944857
Alisalehivalo <55212861+Alisalehivalo@users.noreply.github.com>
2019-12-29 02:53:23 -0800

    1.5 spin added

--- app/src/main/java/ir/airport/emailapp/MainActivity.java
+++ app/src/main/java/ir/airport/emailapp/MainActivity.java
@@ -5,17 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 
-public class MainActivity extends AppCompatActivity {
-EditText mail,cMail,subject,ads,fltTime,ogN,acIdentification,rules,typeFLT,num,typeAC,wTurbulance,equp1,equp2,dep,timeFlt,speed,level,route1,route2,dest,eet,alt1,alt2,other;
-
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+EditText mail,cMail,subject,ads,fltTime,ogN,acIdentification,typeAC,equp1,equp2,dep,timeFlt,speed,level,route1,route2,dest,eet,alt1,alt2,other;
+    String[] rule = { "I","V","Y", "Z" };
+    String[] type = { "S","N","G", "M" ,"I"};
+    String[] turbo = { "L","M","H"};
+    String ruleSelected,typeSelected,turboSelected;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spin_rule = (Spinner) findViewById(R.id.rules1);
+        Spinner spin_type = (Spinner) findViewById(R.id.spin_type);
+        Spinner spin_turbo = (Spinner) findViewById(R.id.spin_turbulance);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, rule);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, type);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, turbo);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin_rule.setAdapter(adapter);
+        spin_type.setAdapter(adapter1);
+        spin_turbo.setAdapter(adapter2);
+        spin_rule.setOnItemSelectedListener(this);
+        spin_type.setOnItemSelectedListener(this);
+        spin_turbo.setOnItemSelectedListener(this);
         mail=findViewById(R.id.email);
         subject=findViewById(R.id.subject);
         //cMail=findViewById(R.id.cEmail);
@@ -23,11 +44,11 @@ EditText mail,cMail,subject,ads,fltTime,ogN,acIdentification,rules,typeFLT,num,t
         fltTime=findViewById(R.id.fillingTime);
         ogN=findViewById(R.id.ogn);
         acIdentification=findViewById(R.id.ident);
-        rules=findViewById(R.id.rules);
-        typeFLT=findViewById(R.id.type);
-        num=findViewById(R.id.num);
+        //rules=findViewById(R.id.rules);
+        //typeFLT=findViewById(R.id.type);
+        //num=findViewById(R.id.num);
         typeAC=findViewById(R.id.typeAC);
-        wTurbulance=findViewById(R.id.wTurbulance);
+        //wTurbulance=findViewById(R.id.wTurbulance);
         equp1=findViewById(R.id.equip1);
         equp2=findViewById(R.id.equip2);
         dep=findViewById(R.id.dep);
@@ -58,11 +79,11 @@ EditText mail,cMail,subject,ads,fltTime,ogN,acIdentification,rules,typeFLT,num,t
                 fltTime.setText("");
                 ogN.setText("");
                 acIdentification.setText("");
-                rules.setText("");
-                typeFLT.setText("");
-                num.setText("");
+                //rules.setText("");
+                //typeFLT.setText("");
+                //num.setText("");
                 typeAC.setText("");
-                wTurbulance.setText("");
+                //wTurbulance.setText("");
                 equp1.setText("");
                 equp2.setText("");
                 dep.setText("");
@@ -88,8 +109,8 @@ EditText mail,cMail,subject,ads,fltTime,ogN,acIdentification,rules,typeFLT,num,t
         String SubjectMail=subject.getText().toString().trim();
         String adress="FF "+ads.getText().toString().toUpperCase()+"\n";
         String origin=fltTime.getText().toString()+" "+ogN.getText().toString().toUpperCase()+"\n";
-        String FPL_MSG="(FPL-"+acIdentification.getText().toString().toUpperCase()+"-"+rules.getText().toString().toUpperCase()+typeFLT.getText().toString().toUpperCase()+"\n"+
-                "-"+num.getText().toString()+typeFLT.getText().toString().toUpperCase()+"/"+wTurbulance.getText().toString().toUpperCase()+"-"+equp1.getText().toString().toUpperCase()+"/"+equp2.getText().toString().toUpperCase()+"\n"+
+        String FPL_MSG="(FPL-"+acIdentification.getText().toString().toUpperCase()+"-"+ruleSelected+typeSelected+"\n"+
+                "-"+typeAC.getText().toString().toUpperCase()+"/"+turboSelected+"-"+equp1.getText().toString().toUpperCase()+"/"+equp2.getText().toString().toUpperCase()+"\n"+
                 "-"+dep.getText().toString().toUpperCase()+timeFlt.getText().toString().toUpperCase()+"\n"+
                 "-"+speed.getText().toString().toUpperCase()+level.getText().toString().toUpperCase()+route1.getText().toString().toUpperCase()+route2.getText().toString().toUpperCase()+"\n"+
                 "-"+dest.getText().toString().toUpperCase()+eet.getText().toString()+" "+alt1.getText().toString().toUpperCase()+" "+alt2.getText().toString().toUpperCase()+"\n"+
@@ -104,5 +125,18 @@ EditText mail,cMail,subject,ads,fltTime,ogN,acIdentification,rules,typeFLT,num,t
             //JavaMailAPI javaMailAPI = new JavaMailAPI(this, addressee,"akbarsalehi@gmail.com", SubjectMail, txtMail);
             //javaMailAPI.execute();
         //}
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        ruleSelected=rule[position];
+        typeSelected=type[position];
+        turboSelected=turbo[position];
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
